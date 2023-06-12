4d2d5e1b7f38858c232047e73d6ba14ba76201dc
TaufikTopan <matjosep000@gmail.com>
2023-06-12 08:40:23 +0700

    Membuat Tampilan LOGIN, DAFTAR INVENTARIS, TAMBAH & EDIT

---
+++ page/list_barang.php
@@ -0,0 +1,66 @@
+<h2><center>DAFTAR INVENTARIS</center></h2>
+<hr>
+<a href="?p=tambah_barang" class=" btn btn-md btn-primary"><span class="glyphicon glyphicon-plus"></span></a>
+<form class="navbar-form navbar-right" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Cari Barang">
+        </div>
+        <button type="submit" class="btn btn-default">Cari</button>
+      </form>
+<br><br>
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Kode Inventaris</th>
+            <th>Nama Barang</th>
+            <th>Kondisi</th>
+            <th>Jumlah</th>
+            <th>Ruang</th>
+            <th>Tanggal Register</th>
+            <th>Keterangan</th>
+            <th>Opsi</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>L001</td>
+            <td>Laptop</td>
+            <td>Baik</td>
+            <td>50</td>
+            <td>LAB RPL</td>
+            <td>19-11-2022</td>
+            <td>Barang di dapat dari bantuan provinsi</td>
+            <td>
+                <a href="?p=edit_barang" class="btn btn-md btn-primary"><span class="glyphicon glyphicon-edit"></span></a>
+                <a href="" class="btn btn-md btn-danger"><span class="glyphicon glyphicon-trash"></span></a>
+            </td>
+        </tr>
+    </tbody>
+</table>
+
+<div class="float-left">
+    Jumlah : 1
+</div>
+<div class="" style="float:right">
+<nav>
+  <ul class="pagination">
+    <li>
+      <a href="#" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>
+    <li><a href="#">1</a></li>
+    <li><a href="#">2</a></li>
+    <li><a href="#">3</a></li>
+    <li><a href="#">4</a></li>
+    <li><a href="#">5</a></li>
+    <li>
+      <a href="#" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
+  </ul>
+</nav>
+</div>
